da936b0686748cc1220709e27e80abaa07c4b078
Andrey Maksyutenko <klikinstall@rambler.ru>
2017-03-04 23:06:03 +0500

    Полностью поменял сайт
    Полностью поменял сайт, теперь это мое портфолио

--- js/common.js
+++ js/common.js
@@ -1,111 +1,67 @@
 $(document).ready(function() {
 
-	//Подключение owlcarousel
-	$(".owl-carousel").owlCarousel({
-			loop:true,
-			responsive : {
-			        0 : {
-			            items:1,
-			            nav:true
-			        }
-			},
-			navText: ""
-		});
-	//Запуск стеллар (параллакс-эффект)
-	$.stellar({
-		responsive: true,
-		horizontalOffset: 60
+	//Анимация плиток в блоке "Резюме"
+	$('#portfolio_grid').mixItUp();
+	//Анимация при клике на элементы списка в блоке "Портфолио"
+	$(".s_portfolio li").click(function(){
+		$(".s_portfolio li").removeClass("active");
+		$(this).addClass("active");
 	});
-	//Изменение размеров фона шапки при изменении размера окна
-	function windowResize(){
-		$('header').css('min-height', $(window).height())
-	};
-	windowResize();
-	$(window).resize(function(){
-		windowResize();
-	});
-
-	//Модальные окна magnific popup
+//Окошки, всплывающие при нажатии
 	$('.popup').magnificPopup({type:'image'});
-	$('.popup_c').magnificPopup();
-
-	//Переключатель вкладок c телефонами
-	$(".top_phone .tab:first-child").addClass('active');
-	$(".top_phone .tab_item").not(":first").hide();
-	$(" .top_phone .tab").click(function() {
-		$(".top_phone .tab").removeClass("active")
-		.eq($(this).index()).addClass("active");
-		$(".top_phone .tab_item").hide().eq($(this).index()).fadeIn()})
-		.eq(0).addClass("active");
-	//Переключатель вкладок c телефонами в футере
-	$(".bottom_phone .tab:first-child").addClass('active');
-	$(".bottom_phone .tab_item").not(":first").hide();
-	$(" .bottom_phone .tab").click(function() {
-		$(".bottom_phone .tab").removeClass("active")
-		.eq($(this).index()).addClass("active");
-		$(".bottom_phone .tab_item").hide().eq($(this).index()).fadeIn()})
-		.eq(0).addClass("active");
-	//Переключатель вкладок со скидками
-	$(".tabs_header .tab:first-child").addClass('active');
-	$(".tabs_header .tab_item").not(":first").hide();
-	$(".tabs_header .wrapper .tab").click(function() {
-		$(".tabs_header .wrapper .tab").removeClass("active")
-		.eq($(this).index()).addClass("active");
-		$(".tabs_header .tab_item").hide().eq($(this).index()).fadeIn()})
-		.eq(0).addClass("active");
-	//Переключатель вкладок с адресами
-	$(".s_contacts .tab:first-child").addClass('active');
-	$(".s_contacts .tab_item").not(":first").hide();
-	$(".s_contacts .tab").click(function() {
-		$(".s_contacts .tab").removeClass("active")
-		.eq($(this).index()).addClass("active");
-		$(".s_contacts .tab_item").hide().eq($(this).index()).fadeIn()})
-		.eq(0).addClass("active");
-
-
-	//Цели для Яндекс.Метрики и Google Analytics
-	$(".count_element").on("click", (function() {
-		ga("send", "event", "goal", "goal");
-		yaCounterXXXXXXXX.reachGoal("goal");
-		return true;
-	}));
-
-	//SVG Fallback
-	if(!Modernizr.svg) {
-		$("img[src*='svg']").attr("src", function() {
-			return $(this).attr("src").replace(".svg", ".png");
-		});
+	$('.popup_content').magnificPopup({type:'inline'});
+//Анимация имени и заголовков секций
+	$('.top_text h1').animated("fadeInDown","fadeOutUp");
+	$('.top_text p, .section_header').animated("fadeInUp","fadeOutDown");
+//Анимация параграфов и фотки
+	$('.animation_1').animated("flipInY","flipOutY");
+	$('.animation_2').animated("fadeInLeft","fadeOutLeft");
+	$('.animation_3').animated("fadeInRight","fadeOutRight");
+	//Анимация блоков резюме
+	$(".left .resume_item").animated("fadeInLeft","fadeOutLeft");
+	$('.right .resume_item').animated("fadeInRight","fadeOutRight");
+//Подгонка бэкграунда под высоту окна
+	function heightDetect(){
+		$(".main_head").css("height",$(window).height());
 	};
-
-	//Аякс отправка форм
-	//Документация: http://api.jquery.com/jquery.ajax/
-	$("form").submit(function(e) {
-		var ths = $(this);
-		e.preventDefault;
-		$.ajax({
-			type: "POST",
-			url: "mail.php",
-			data: $(this).serialize()
-		}).done(function() {
-			alert("Спасибо за заявку!");
-			setTimeout(function() {
-				var magnificPopup = $.magnificPopup.instance;
-				magnificPopup.close();
-				ths.trigger('reset');
-			}, 1000);
-		});
-		return false;
+	heightDetect();
+	$(window).resize(function(){
+		heightDetect();
 	});
-
+/*Анимация крестика*/
+	$(".toggle_mnu").click(function() {
+		$(".sandwich").toggleClass("active");
+	});
+//
+	$(".top_mnu li a").click(function(){
+		$(".top_mnu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
+	}).append("<span>");
+//Всплывающее меню при нажатии на сэндвич
+	$(".toggle_mnu").click(function() {
+		if ($(".top_mnu").is(":visible")){
+			$(".top_text").removeClass("h_opacity");
+			$(".top_mnu").fadeOut(600);
+			$(".top_mnu li a").removeClass("fadeInUp animated");
+		}
+		else {
+			$(".top_text").addClass("h_opacity");
+			$(".top_mnu").fadeIn(600);
+			$(".top_mnu li a").addClass("fadeInUp animated");
+		};
+	});
+	//Присвоение айди плиткам в блоке "Портфолио"
+	$(".portfolio_item").each(function(i){
+		$(this).find("a").attr("href","#work_"+i);
+		$(this).find(".port_descr").attr("id","work_"+i);
+	});
+	//Валидация форм
+	$("input,select,textarea").jqBootstrapValidation();
+	//Плавный скролл по странице
+	$(".top_mnu ul a").mPageScroll2id();
 });
-
+	//Загрузочный экран
 $(window).load(function() {
-	//Анимация элементов на странице
-	$(".top_header h1").animated("fadeInDown", "fadeInDown");
-	$(".top_header h2").animated("fadeInUp", "fadeInUp");
-	$(".tabs_header .wrapper").animated("flipInY", "flipInY");
-	$(".prof_item").animated("fadeInRight", "fadeInRight");
-	$(".s_prof form").animated("zoomInRight", "zoomInRight");
-	$(".s_back h3").animated("fadeInUp", "fadeInUp");
-	$("footer").animated("fadeIn", "fadeIn");
+	$(".loaderInner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
 });
